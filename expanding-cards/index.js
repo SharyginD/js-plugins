2a73939241e09249bbff32572f88fbad27a26830
Дмитрий Шарыгин <dsh@simsales.ru>
2021-06-12 21:07:41 +0300

    expanding-cards

---
+++ expanding-cards/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slide');
+
+    slides.forEach((slide) => {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        })
+    });
+
+    const clearActiveClasses = () => {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
+});
